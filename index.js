f5d7fe165e89ae7097846fc33236fe05e8737816
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-03-20 23:35:34 +0300

    Challenge 1 javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+// Prompt the user to input the student marks 
+const marks = prompt("Enter student marks (between 0 and 100)");
+
+// Check if the input is between 0 and 100
+if (marks >= 0 && marks <= 100) {
+  // Grade based on inputed marks
+  let message = "Student Grade: ";
+  if (marks > 79) {
+    console.log(`${message}A`);
+  } else if (marks >= 60 && marks <= 79) {
+    console.log(`${message}B`);
+  } else if (marks >= 50 && marks <= 59) {
+    console.log(`${message}C`);
+  } else if (marks >= 40 && marks <= 49) {
+    console.log(`${message}D`);
+  } else {
+    console.log(`${message}E`);
+  }
+} else {
+  // Invalid input
+  console.log("Invalid");
+}
