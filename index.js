4f72cfbe517c0c43dbf7a78d4baba9c25cc2fffc
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-03-21 00:33:02 +0300

    Speed Detector

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// Challenge 1: Student Grade Generator
 // Prompt the user to input the student marks 
-const marks = prompt("Enter student marks (between 0 and 100)");
+const marks = prompt("Enter student marks (between 0 and 100): ");
 
 // Check if the input is between 0 and 100
 if (marks >= 0 && marks <= 100) {
@@ -17,6 +18,32 @@ if (marks >= 0 && marks <= 100) {
     console.log(`${message}E`);
   }
 } else {
-  // Invalid input
-  console.log("Invalid");
+  // Input is not between 0 and 100
+  console.log("Invalid input");
 }
+
+
+// Challenge 2: Speed Detector
+
+//prompt the user to input car's speed
+const speed = prompt("Enter speed of the car in km/h: ");
+
+//Define speed limit
+const speedLimit = 70;
+//Define demerit point per 5kmh
+const points = 1;
+//Calculate demerit point
+if (speed > 0){
+    const demeritPoints = Math.floor((speed - speedLimit)/5) * points;
+
+    //check if speed is over the limit
+    if (demeritPoints > 12) {
+        console.log("License suspended");
+      } else if (demeritPoints > 0) {
+        console.log(`Points: ${demeritPoints}`);
+      } else {
+        console.log("Ok");
+      }
+    } else {
+      console.log("Invalid input");
+    }
