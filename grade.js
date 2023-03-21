5b7b33f471c39e0834c86e5abfe2796caa7d957a
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-03-21 18:02:18 +0300

    Edit Student Grade Generator

--- grade.js
+++ grade.js
@@ -1,26 +1,28 @@
 // Challenge 1: Student Grade Generator
 // Prompt the user to input the student marks 
-const marks = prompt("Enter student marks (between 0 and 100): ");
-
+function generateGrade() {
+  const marks = document.getElementById("marks").value;
 // Check if the input is between 0 and 100
-if (marks >= 0 && marks <= 100) {
+  if (marks >= 0 && marks <= 100) {
+    let grade = "";
   // Grade based on inputed marks
-  let message = "Student Grade: ";
-  if (marks > 79) {
-    alert(`${message}A`);
-  } else if (marks >= 60 && marks <= 79) {
-    alert(`${message}B`);
-  } else if (marks >= 50 && marks <= 59) {
-    alert(`${message}C`);
-  } else if (marks >= 40 && marks <= 49) {
-    alert(`${message}D`);
-  } else {
-    alert(`${message}E`);
-  }
+  
+    if (marks > 79) {
+      grade = "A";
+    } else if (marks >= 60 && marks <= 79) {
+      grade = "B";
+    } else if (marks >= 50 && marks <= 59) {
+      grade = "C";
+    } else if (marks >= 40 && marks <= 49) {
+      grade = "D";
+    } else {
+      grade = "E";
+    }
+//generator output
+document.getElementById("output").innerHTML = `Student Grade: ${grade}`;
 } else {
-  // Input is not between 0 and 100
-  alert("Invalid input");
+  document.getElementById("output").innerHTML = "Invalid input";
 }
-
+}
 
 
